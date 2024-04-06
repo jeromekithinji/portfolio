be5f2783ba9b8d6efc8b7eb08daab2e7d93c71fa
Jerome Kithinji <kithinjijerome@gmail.com>
2024-04-06 00:20:12 -0400

    Updated experience information

--- src/Assets/data/experienceData.js
+++ src/Assets/data/experienceData.js
@@ -1,29 +1,81 @@
 import projectImage from "../images/skills/adobe-xd.svg";
+import telkom from "../images/experience/Jerome Kithinji LinkedIn logo.jpeg";
+import nology from "../images/experience/Nology IO logo.jpeg";
+import techsavannah from "../images/experience/Techsavanna logo.jpeg";
+import myCredit from "../images/experience/myCreditLogo.jpeg";
+import leicester from "../images/experience/Leicester University logo.jpeg";
+
 
 export const experienceData = [
     {
         type: "work",
-        image: projectImage,
-        title: "Frontend Developer | Freelancer.com",
-        location: "London, UK",
-        date: " May 2019 - Jul 2022",
+        image: telkom,
+        title: "Software Developer | Internship",
+        location: "Nairobi, Kenya | Hybrid",
+        date: "September 2023 - December 2023",
         details: [
-            "Developed web applications using React, Redux, Next.js",
-            "Building a landing page with high speed and high scores in speed test gtmetrix and lighthouse, etc",
-            "Creating a web application (Windows, Android and iOS)",
-            "Create website with modern monorepo architecture",
-            "Collaborated closely with other developers on projects that require multiple people to complete tasks",
+            "Redesigned and redeveloped the Human Resource Management system as a web application using Figma, ReactJS and Node.js.",
+            "Implemented the web application's backend, integrating the company's existing employee API and creating additional endpoints to expand functionality.",
+            "Conducted extensive performance testing with Apache JMeter to enhance application efficiency, ensuring smooth operation for widespread company use by simulating high-traffic loads.",
+            "Ensured cross-platform reliability and responsiveness by executing comprehensive browser compatibility tests using Selenium WebDriver.",
+            "Operated in an Agile framework in a team of 30 people, actively participating in daily standups and weekly sprint reviews.",
+            "Worked with the Networking department on on-site data center operations, troubleshooting network systems, and configuring network hardware like routers and switches.",
+        ],
+    },
+    {
+        type: "work",
+        image: nology,
+        title: "Full Stack Developer",
+        location: "London, UK | Remote",
+        date: "February 2022 - March 2023",
+        details: [
+            "Working as a Full-Stack Developer at SparkStrand, a digital agency.",
+            "Developed and launched multiple custom web applications for clients across different industries.",
+            "Collaborated with cross-functional teams including designers, project managers, and clients.",
+            "Integrated multiple third-party APIs and services, including payment gateways, social media platforms, and marketing tools, improving website functionality and user experience.",
+            "Successfully led the development of a large-scale e-commerce website for a major retail client, which significantly increased their online sales and customer engagement.",
+            "Designed and implemented robust content management systems (CMS) for clients, which allowed them to easily update and manage their website content, resulting in improved user experience and increased website traffic.",
+            "Implemented various web analytics and tracking tools, including Google Analytics and Mixpanel, to help clients measure and improve their online performance.",
+            "Led the development of a reusable React component library, which reduced development time and improved consistency and scalability of client projects.",
+            "Implemented SEO best practices and optimized website performance, resulting in an increase in website traffic and user engagement on clients' websites.",
+        ],
+    },
+    {
+        type: "work",
+        image: techsavannah,
+        title: "Softwarer Developer | Internship",
+        location: "Nairobi, Kenya | Remote",
+        date: "December 2021 - April 2022",
+        details: [
+            "Successfully re-designed and updated a mobile banking web application for a major bank in Kenya resulting in a significant improvement in user experience.",
+            "Created comprehensive documentation and conducted training sessions for customers and operational support personnel, resulting in increased efficiency and better support for end-users.",
+            "Collaborated remotely with an agile team located in different parts of Kenya to translate the SRS platform from Angular to React, refactor outdated SQL queries and modernize selenium tests.",
+        ],
+    },
+    {
+        type: "work",
+        image: myCredit,
+        title: "Information Technology Intern| Internship",
+        location: "Nairobi, Kenya",
+        date: "June 2019 - August 2019",
+        details: [
+            "Maintaining the bank's computer systems, servers, and networks to ensure they are always operational and up-to-date.",
+            "Providing technical support to the bank's employees to ensure they can work efficiently and productively.",
+            "Collaborating with other IT professionals and stakeholders to identify and solve complex technical issues.",
+            "Training new employees on the bank's IT systems and software to ensure they can work effectively.",
+            "Overseeing IT-related projects from inception to completion, ensuring that they are delivered on time and within budget.",
+            "Improving the bank's IT infrastructure by suggesting and implementing innovative solutions to increase efficiency and productivity.",
         ],
     },
     {
         type: "education",
-        image: projectImage,
-        title: "Frontend Developer | Upwork",
-        date: "Sept 2019 - Jan 2022",
+        image: leicester,
+        title: "BSc Computer Science | First Class Honours",
+        location: "University of Leicester, UK",
+        date: "September 2018 - July 2021",
         details: [
-            "Worked with a team of 5 developers to create an e-commerce website using React and NodeJS",
-            "Built a dashboard for monitoring server performance using NodeJS, React, Material-ui",
-            "Created reusable components that were easily maintainable, reducing the time spent on future development by 50%",
+            "I was the vice president of the East African Society from August 2020 - July 2021, where I organized and led diverse events like game nights and food parties, attracting over 90 attendees, and enhanced community ties through cross-cultural initiatives. I managed a dedicated team to deliver successful events, oversaw the society's social media for broader engagement, and worked with the President to strategize membership growth. Additionally, I established connections with local businesses and community groups, securing sponsorships to support our activities.",
+            "As a student ambassador from March 2019 to August 2020, I represented the university at various events and fairs, introducing the institution to potential students and partners, and provided campus tours detailing academic and student life. I was involved in organizing and executing recruitment events, collaborated on outreach and marketing initiatives, and offered tailored support to new and prospective students, showcasing effective communication and problem-solving abilities.",
         ],
     },
 ];
